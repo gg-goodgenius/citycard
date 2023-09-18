febf1b8269fab0a3d965dc2ff4186f0caddefbb2
Maksud <leylakarimova@mail.ru>
2023-09-18 15:30:04 +0300

    Added message healthcheck

--- apps/api/internal/controllers/http/v1/healthcheck.go
+++ apps/api/internal/controllers/http/v1/healthcheck.go
@@ -5,9 +5,10 @@ import "github.com/gin-gonic/gin"
 type HealthcheckHandler struct{}
 
 func (h *HealthcheckHandler) Register(e *gin.Engine) {
+	e.Group("/healthcheck", CORSMiddleware())
 	e.GET("/healthcheck", h.Healthcheck)
 }
 
 func (h *HealthcheckHandler) Healthcheck(c *gin.Context) {
-	c.Status(200)
+	c.JSON(200, gin.H{"message": "ok"})
 }
